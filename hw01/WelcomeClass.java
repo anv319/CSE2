8169b779e2b1cb8f78a1fe249b96b7a94bf9592b
anv319 <anv319@lehigh.edu>
2015-08-29 23:10:57 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,25 @@
+/////////////////////////////////////////////////////////////////////
+//Anthony Varshavskiy
+//HW 01
+//Welcome Class Program
+
+// define a class
+public class WelcomeClass{
+    
+//  add main method   
+    public static void main(String[] args) {
+        
+//  print Welcome statement with tweet-bio
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-A--N--V--3--1--9->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println("This summer I was able to enjoy a variety of summer activities.");
+    System.out.println("I swam in a lake and pool, played basketball, and went to a festival.");
+        
+    }
+}
